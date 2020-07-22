0877a8563c7cb43b94c46bbdb5ff1f350581edcf
santiago <ssantiagotl@gmail.com>
2020-07-21 23:42:34 -0300

    byte, short, long and width

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // ----------------Integers---------------- //
+
         int myValue = 10000;
 
         int myMinIntValue = Integer.MIN_VALUE;
@@ -12,5 +14,32 @@ public class Main {
         System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Minimum Value = " + myMaxIntValue);
 
+        // ----------------Byte---------------- //
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Minimum Value = " + myMaxByteValue);
+
+        // ----------------Short---------------- //
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Minimum Value = " + myMaxShortValue);
+
+        // ----------------Long---------------- //
+
+        long myLongValue = 100L;
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Minimum Value = " + myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_648_234L;
+        System.out.println(bigLongLiteralValue);
     }
 }
