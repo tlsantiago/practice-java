de4b4837690d3eeaf57ec473a7d470ace4c58c76
santiago <ssantiagotl@gmail.com>
2020-07-21 22:51:23 -0300

    Started using variables with operators

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,8 +3,13 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        int myFirstNumber = 5;
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
 
-        System.out.println(myFirstNumber);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
