2ea99db7a82351cd68877a854878d900ddc2d3ea
santiago <ssantiagotl@gmail.com>
2020-07-23 01:00:21 -0300

    Operators and abbreviation

--- operators/src/com/company/Main.java
+++ operators/src/com/company/Main.java
@@ -12,6 +12,29 @@ public class Main {
         System.out.println("3 - 1 = " + result);
         System.out.println("previusResult = " + previousResult);
 
+        result = result * 10;
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5;
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3;
+        System.out.println("4 % 3 = " + result);
+
+        result++;
+        System.out.println("1 + 1 = " + result);
+
+        result--;
+        System.out.println("2 - 1 = " + result);
+
+        result *= 10;
+        System.out.println("1 * 10 = " + result);
+
+        result /= 3;
+        System.out.println(result);
+
+        result -= 2;
+        System.out.println("10 - 2 = " + result);
 
     }
 }
