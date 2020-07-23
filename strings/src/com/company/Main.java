67e106b1803cc8e5403f6753b9b47f7974d02111
santiago <ssantiagotl@gmail.com>
2020-07-23 00:37:04 -0300

    Recaping primitive types

---
+++ strings/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //byte
+        //short
+        //int
+        //long
+        //float
+        //double
+        //char
+        //boolean
+
+        String myString = "This is a string";
+        System.out.println("MyString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("MyString is equal to " + myString);
+        myString = myString + " \u00A9 2019";
+        System.out.println("MyString is equal to " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
+
+
+    }
+}
