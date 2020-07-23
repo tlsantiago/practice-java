36ef2547da58b1cd5801ffabab3b2e22ee343ee5
santiago <ssantiagotl@gmail.com>
2020-07-23 11:19:38 -0300

    Understanding Keywords and Expressions

---
+++ keywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int int2 = 5;
+        //false, true, null
+        int highScore = 50;
+
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+    }
+}
