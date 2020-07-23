dd5845b1684cde6aa67e548c259c502b6c0e69d9
santiago <ssantiagotl@gmail.com>
2020-07-23 00:55:36 -0300

    Simple operators

---
+++ operators/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2;
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previusResult = " + previousResult);
+        result = result -1;
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previusResult = " + previousResult);
+
+
+    }
+}
