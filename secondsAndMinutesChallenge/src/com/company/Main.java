cf2b93058023ac98cb3125389f15f8c6b38efbe0
santiago <ssantiagotl@gmail.com>
2020-07-24 01:47:00 -0300

    Declared new variables to prevent overwriting given parameters

--- secondsAndMinutesChallenge/src/com/company/Main.java
+++ secondsAndMinutesChallenge/src/com/company/Main.java
@@ -9,30 +9,30 @@ public class Main {
         getDurationString(6009);
     }
 
-    public static String getDurationString(int minutes, int seconds) {
+    public static String getDurationString(long minutes, long seconds) {
 
         if (minutes < 0 || seconds < 0 || seconds > 59 ) {
             System.out.println("Invalid Value");
             return "Invalid Value";
         } else {
             long hours = minutes / 60;
-            minutes = minutes % 60;
-            seconds = seconds % 60;
-            System.out.println(hours + "h " + minutes + "m " + seconds +
+            long remainingMinutes = minutes % 60;
+            long remainingSeconds = seconds % 60;
+            System.out.println(hours + "h " + remainingMinutes + "m " +remainingSeconds +
                     "s");
             return hours + "h " + minutes + "m " + seconds +
                     "s";
         }
     }
 
-    public static String getDurationString(int seconds) {
+    public static String getDurationString(long seconds) {
         if (seconds < 0) {
             System.out.println("Invalid Value");
             return "Invalid Value";
         } else {
-            int minutes = seconds / 60;
-            seconds = seconds % 60;
-            return getDurationString(minutes, seconds);
+            long minutes = seconds / 60;
+            long remainingSeconds = seconds % 60;
+            return getDurationString(minutes, remainingSeconds);
         }
     }
 
