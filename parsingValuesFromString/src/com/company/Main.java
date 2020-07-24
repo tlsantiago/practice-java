c9e26b0090c2ba226fb93a3a3ad10e4c6cb4a419
santiago <ssantiagotl@gmail.com>
2020-07-24 16:39:36 -0300

    Parsing values from string

---
+++ parsingValuesFromString/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String numberAsString = "2018";
+        System.out.println("NumberAsString = "  + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("number = " + number);
+    }
+}
