d34bc8ee1a4c1b87bd10c34c19cfb7657ccb1077
santiago <ssantiagotl@gmail.com>
2020-07-24 12:32:29 -0300

    For loops

---
+++ forLoop/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Loop " + i + " hello!");
+        }
+
+        for (int i=2; i < 9; i++) {
+            System.out.println("10.000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0,i)));
+        }
+
+        System.out.println("##################################");
+
+        for (int i=8; i >= 2; i--) {
+            System.out.println("10.000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0,i)));
+        }
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount *(interestRate/100));
+    }
+}
